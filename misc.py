17ec63553eec2366f70b8183bd9dad209d77ba15
Dasun <sendtodasun@gmail.com>
2025-12-17 18:51:30 +0530

    Initial commit

---
+++ misc.py
@@ -0,0 +1,12 @@
+class spinner:
+
+    characters = ('|','/','-','\\')
+
+    def __init__(self):
+        self.print_turning_pipe = 0
+        pass
+
+    def printtp(self):
+        print(spinner.characters[self.print_turning_pipe],end='',flush=True)
+        self.print_turning_pipe=(self.print_turning_pipe+1)%4
+        print('\b',end='',flush=True)
---
+++ test_misc.py
@@ -0,0 +1,31 @@
+# test_misc.py
+import time
+from misc import spinner
+
+def test_spinner():
+    t = 1
+
+    print(f"Test {t}: Simulated long task")
+    print("Processing...", end="", flush=True)
+    s = spinner()
+    for _ in range(80):
+        s.printtp()
+        time.sleep(0.05)
+    print("\bDone!")
+
+    t+=1
+    print(f"\nTest {t}: Reusable progress function")
+    def show_progress(duration=4, message="Working"):
+        print(f"{message}... ", end="", flush=True)
+        s = spinner()
+        steps = int(duration / 0.1)
+        for _ in range(steps):
+            s.printtp()
+            time.sleep(0.05)
+        print("\bDone!")
+
+    show_progress(2, "Loading data")
+    show_progress(4, "Training model")
+    show_progress(3, "Saving file")
+
+test_spinner()
