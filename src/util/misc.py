913d9d90f29429e9de293bd04236fa01f3d5f2f5
Dasun <sendtodasun@gmail.com>
2025-12-18 10:57:17 +0530

    cr for notebooks

--- src/util/misc.py
+++ src/util/misc.py
@@ -9,4 +9,4 @@ class spinner:
     def printtp(self):
         print(spinner.characters[self.print_turning_pipe],end='',flush=True)
         self.print_turning_pipe=(self.print_turning_pipe+1)%4
-        print('\b',end='',flush=True)
+        print('\b\r',end='',flush=True)
